3a568406506cc1293ab2f7bba1bde74d81bdbec4
hyukssang <hyukssang@gmail.com>
2016-04-22 15:50:07 -0400

    Final check before submission

--- CommutHer/ProfileController.swift
+++ CommutHer/ProfileController.swift
@@ -7,16 +7,57 @@
 //
 
 import UIKit
+import Parse
 
 class ProfileController: UIViewController {
 
     @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var phonenum: UITextField!
+    @IBOutlet weak var school: UITextField!
+    
+    @IBAction func saveProfile(sender: AnyObject) {
+        
+        if self.password.text != ""{
+            PFUser.currentUser()?.setObject(self.password.text!, forKey: "password")
+        }
+        if self.email.text != ""{
+            PFUser.currentUser()?.setObject(self.email.text!, forKey: "email")
+        }
+        if self.phonenum.text != ""{
+            PFUser.currentUser()?.setObject(self.phonenum.text!, forKey: "phonenum")
+        }
+        if self.school.text != "" {
+            PFUser.currentUser()?.setObject(self.school.text!, forKey: "school")
+        }
+        
+        PFUser.currentUser()?.saveInBackground()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.profilePic.image = UIImage(named: "profile")
+        self.username.text = PFUser.currentUser()!.username
+        self.password.placeholder = PFUser.currentUser()!.password
+        self.email.placeholder = PFUser.currentUser()!.email
+        self.phonenum.placeholder = PFUser.currentUser()!.objectForKey("phonenum") as? String
+        self.school.placeholder = PFUser.currentUser()!.objectForKey("school") as? String
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
